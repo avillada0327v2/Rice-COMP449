5f0dddbb9e6b908279c99afee502f22ea15d4cf5
JudyFang <57746866+judyfang0108@users.noreply.github.com>
2024-03-26 15:00:08 -0500

    Update bert_embeddings.py

--- model/bert_embeddings.py
+++ model/bert_embeddings.py
@@ -1,10 +1,21 @@
-import pandas as pd
 import pickle
 import gc  # Garbage Collector interface
 import os
 import torch
+import random
+import numpy as np
+import pandas as pd
 from transformers import BertTokenizer, BertModel
-from model.utils import *
+
+def set_seeds(seed=42):
+    """Set seeds for reproducibility."""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if using multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 def process_text(tokenizer, model, context):
     """
